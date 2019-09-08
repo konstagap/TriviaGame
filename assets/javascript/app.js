709242cf3a933eb71aa76fc251229f8c10d57dc1
konstagap <konstagapov123@gmail.com>
2019-09-07 17:09:22 -0700

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,119 +1,186 @@
 
-//create array of objects {q:---, choice..., a:---}
+//create array of objects 
 var questions = [
-    { question: "is it hard?", answer: 'Yes', },
-    { question: "is snow red?", answer: 'No',},
-    { question: "how many weeks in a month?", answer: '4', }
-    
+    { question: "is it hard?", answer: 'Yes', wrong: "No", },
+    { question: "is snow red?", answer: 'No', wrong: "Yes", },
+    {
+        question: "how many weeks in a month?", answer: '4',
+        wrong: "5", wrong2: "14",
+    }
+
 ]
 //we will assign value of this var when user click on radio button
 var userChoice;
-// '<input type="radio" name="question1" value="yes">
-var userChoice2
-var userChoice3
+var userChoice2;
+var userChoice3;
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var noAnswer = 0;
+/// Vars for our count down and timer show
+var intervalId;
+var timeleft = 30;
+
 // we letting our html load before we do any modification with dom
 $(document).ready(function () {
-
+// user click on stat and magic begins
     $("#start").on("click", function () {
-        $(this).remove()
+        //Submit button pops up on screen
+        $(".boxButton").append(submitButton)
+        ///TIMER FUNCTION.../////////////////////
+        $('.display').html("Seconds left: " + timeleft)
+        intervalId = setInterval(count, 1000);
+        function count() {
+            /// we going to check our var timeleft, -1 every second, show its value on screen every second until its 0! 
+            if (timeleft !== 0) {
+                timeleft--;
+                $('.display').html("Seconds left: " + timeleft)
+            } else {
+                // when its zero we gonna perform out "submit button function" to collect stats and show them, which i supposed to put to global scope
+                //and reuse here and when press submit button, i tried but im realy doing smthng wrong and its not working right...
+                //clearing interval, showing results
+                clearInterval(intervalId)
+                collectStats(userChoice, 0);
+                collectStats(userChoice2, 1);
+                ///third question has 3 choices so previous function will give us wrong stats if we run it.
+                if (userChoice3 == questions[2].answer) {
+                    rightAnswers++;
+                } else if ((userChoice3 == questions[2].wrong) || (userChoice3 == questions[2].wrong2)) {
+                    wrongAnswers++;
+                } else {
+                    noAnswer++;
+                }
+                // we empty screen and showing results, if all right we say U GOT THEM ALL
+                // i should have put this in the function and reuse it again in else, but I am doing smthing wrong everytime im using functions
+                $('.boxButton').empty()
+                if ((userChoice == questions[0].answer) && (userChoice2 == questions[1].answer) && (userChoice3 == questions[2].answer)) {
+                    var finalScreen = $('<div>')
+                    var winner = $("<h1>").text("!!!!!!YOU GOT THEM ALL!!!!!!")
+                    finalScreen.append(winner)
+                    var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
+                        " You have " + wrongAnswrse + " answers! " + " You left " + noAnswer + " with no answer! ")
+                    finalScreen.append(stats)
+                    $("#finalscreen").append(finalScreen)
+                }
+                else {
+                    var finalScreen = $('<div>')
+                    var loser = $("<h1>").text("!!!TRY AGAIN!!!!")
+                    finalScreen.append(loser)
+                    var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
+                        " You have " + wrongAnswers + " wrong answers! " + " You left " + noAnswer + " with no answer! ")
+                    finalScreen.append(stats)
+                    $("#finalscreen").append(finalScreen)
+                }
 
+            }
+        }
+
+        $(this).hide()
         //QUestion one!
         var questionDiv = $('<div>')
         $(".boxButton").append(questionDiv)
         $(questionDiv).html(questions[0].question + '<br>' +
             '<input id="first" type="radio" name="question1" value="Yes"> Yes' +
             '<input id="first" type="radio" name="question1" value="No"> No')
-
         $("input[id='first']").on("click", function () {
-            // when we capture user choice we can get value from radio button.
+            //  get value from radio button.
             userChoice = $(this).attr("value")
             console.log(userChoice)
-            // - IF COMPUTER ANSWER AND USER ANSWER MATCH THEN SHOW "ITS CORRECT" 
-            if (userChoice == questions[0].answer) {
-                console.log("you are right")
-            } else {
-                console.log("you are wrong")
-            }
+
         })
         //QUestion two!
         var questionDiv1 = $('<div>')
         $(".boxButton").append(questionDiv1)
         $(questionDiv1).html(questions[1].question + '<br>' +
-            '<input id="second" type="radio" name="question1" value="Yes"> Yes' +
-            '<input id="second" type="radio" name="question1" value="No"> No')
+            '<input id="second" type="radio" name="question2" value="Yes"> Yes' +
+            '<input id="second" type="radio" name="question2" value="No"> No')
         $("input[id='second']").on("click", function () {
-            // when we capture user choice we can get value from radio button.
+            // get value from radio button.
             userChoice2 = $(this).attr("value")
             console.log(userChoice2)
-            // - IF COMPUTER ANSWER AND USER ANSWER MATCH THEN SHOW "ITS CORRECT" 
-            if (userChoice2 == questions[1].answer) {
-                console.log("you are right")
-            } else {
-                console.log("you are wrong")
-            }
         })
         //Question three!
         var questionDiv2 = $('<div>')
         $(".boxButton").append(questionDiv2)
         $(questionDiv2).html(questions[2].question + '<br>' +
-            '<input id="third" type="radio" name="question1" value="4"> 4' +
-            '<input id="third" type="radio" name="question1" value="5"> 5' +
-            '<input id="third" type="radio" name="question1" value="14"> 15')
+            '<input id="third" type="radio" name="question3" value="4"> 4' +
+            '<input id="third" type="radio" name="question3" value="5"> 5' +
+            '<input id="third" type="radio" name="question3" value="14"> 15')
         $("input[id='third']").on("click", function () {
-            // when we capture user choice we can get value from radio button.
+            // get value from radio button.
             userChoice3 = $(this).attr("value")
             console.log(userChoice3)
-            // - IF COMPUTER ANSWER AND USER ANSWER MATCH THEN SHOW "ITS CORRECT" 
-            if (userChoice3 == questions[2].answer) {
-                console.log("you are right")
-            } else {
-                console.log("you are wrong")
-            }
+            // console.log("right: "+rightAnswers)
+            // console.log("wrong: " + wrongAnswers)
+            // console.log("no answer: "+ noAnswer)
         })
-
     })
 
+    //SUBMIT BUTTON//
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var submitButton = $("<button>").text('Submit')
+    // $(submitButton).on("click",
+    $(submitButton).on("click", function () {
+        clearInterval(intervalId);
+        console.log("help")
+        collectStats(userChoice, 0);
+        collectStats(userChoice2, 1);
+        if (userChoice3 == questions[2].answer) {
+            rightAnswers++;
+        } else if ((userChoice3 == questions[2].wrong) || (userChoice3 == questions[2].wrong2)) {
+            wrongAnswers++;
+        } else {
+            noAnswer++;
+        }
+        $('.boxButton').empty()
+        if ((userChoice == questions[0].answer) && (userChoice2 == questions[1].answer) && (userChoice3 == questions[2].answer)) {
+            var finalScreen = $('<div>')
+            var winner = $("<h1>").text("!!!!!!YOU GOT THEM ALL!!!!!!")
+            finalScreen.append(winner)
+            var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
+                " You have " + wrongAnswers + " answers! " + " You left " + noAnswer + " with no answer! ")
+            finalScreen.append(stats)
+            $("#finalscreen").append(finalScreen)
+        }
+        else {
+            var finalScreen = $('<div>')
+            var loser = $("<h1>").text("!!!TRY AGAIN!!!!")
+            finalScreen.append(loser)
+            var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
+                " You have " + wrongAnswers + " wrong answers! " + " You left " + noAnswer + " with no answer! ")
+            finalScreen.append(stats)
+            $("#finalscreen").append(finalScreen)
+        }
+    })
+    function collectStats(userchoice, i) {
+        if (userchoice == questions[i].answer) {
+            rightAnswers++;
+        }
+        else if (userchoice == questions[i].wrong) {
+            wrongAnswers++;
+        }
+        else {
+            noAnswer++;
+        }
+    }
 })
-//        //SECOND QUESTION!
-//        var questionDiv2 = $('<div>')
-//         $(".boxButton").append(questionDiv2)
-//         //target html element and replace it with new html element that shows our questions 
-//         // and radio buttons
-//         $(questionDiv2).html(questions[1].q +'<br>'+
-//         '<input id="button2" type="radio" name="question2" value="bad"> Bad '+
-//         '<input id="button2" type="radio" name="question2" value="good"> Good '+
-//         '<input id="button2" type="radio" name="question2" value="bad"> Bad '
-//        )
-//        $("input").on("click", function(){
-//            // when we capture user choice we can get value from radio button.
-//            userChoice = $(this).attr("value") 
-//            console.log(userChoice)
-//            // - IF COMPUTER ANSWER AND USER ANSWER MATCH THEN SHOW "ITS CORRECT" 
-//            if (userChoice == questions[1].a) {
-//                console.log ("you are right")
-//            } else {
-//                console.log ("you are wrong")
-//            }
 
-    // <!-- WHEN START CLICKED:
-    //     1. HTML SHOWS QUESTION AND RADIO BUTTON TO ANSWER
-    //         - HTML CHANGES WITH LIST OF QUESTIONS
-    //                      -create 3 input radio, give them value
+    //Submitting answers
+    // когда юзер нажал сабмит:
+    // -спрятать екран с вопросами
+    // -на чистом екране показать
+    //     -если все ответы правильные то показать на екране ( ВСЕ ПРАВИЛЬНО, ТЫ ПОБЕДИЛ!)
+    //         - кол-во правильных ответов.
+    //         - кол-во не правильных ответов.
+    //         -кол-во не отвеченых ответов
+    //     -если не все ответы правильны то показать (Ты проиграл!)
+    //         - кол-во правильных ответов.
+    //         - кол-во не правильных ответов.
+    //         -кол-во не отвеченых ответов
 
 
-    //         - RADIO BUTTON FOR USER INTERACTION
-    //         - IF COMPUTER ANSWER AND USER ANSWER MATCH THEN SHOW "ITS CORRECT" 
-    //             ELSE - ITS WRONG!
-//     2.TIMER COUNTS DOWN 120 SECS
-//         SET TIMEOUT FUNCTION TO CHANGE HTML IN 120 SECS.
-//             WHEN FUNCTION RUNS IT SHOULD CHECK IF USER GUESS MATCHES COMPUTER,
-//              SHOW HTLM WITH QUESTIONS AND BELOW THEM STATEMENTS "RIGHT" "WRONG"
-//              SHOW STATEMENT THAT TIME RUN OUT.
-//              CLEARTIMEOUT.
-//              RESTART BUTTON POPS OUT
-//     3.IF "DONE" CLICKED GAMES STOPS
-//         CLEARTIMEOUT, RUN FUNCTION CHECK IF USER GUESS MATCHES COMPUTER,
-//              SHOW HTLM WITH QUESTIONS AND BELOW THEM STATEMENTS "RIGHT" "WRONG"
-//              SHOW STATEMENT LETS SEE YOUR RESULT.
-//              RESTART BUTTON POPS OUT
-//     4.PRESS RESTART
+
+
+
+
+
