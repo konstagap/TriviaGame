3dbcf470ef8c4b365e35c6eaa6add9ad6571bea0
konstagap <konstagapov123@gmail.com>
2019-09-08 12:29:08 -0700

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,27 +7,26 @@ var questions = [
         question: "How many weeks in a month?", answer: '4',
         wrong: "5", wrong2: "14",
     }
-
 ]
 //we will assign value of this var when user click on radio button
 var userChoice;
 var userChoice2;
 var userChoice3;
+//this var made to keep track of user answers
 var rightAnswers = 0;
 var wrongAnswers = 0;
 var noAnswer = 0;
-/// Vars for our count down and timer show
+/// Vars for our count down 
 var intervalId;
 var timeleft = 30;
-
 // we letting our html load before we do any modification with dom
 $(document).ready(function () {
-// user click on stat and magic begins
+    // user click on stat and magic begins
     $("#start").on("click", function () {
         //Submit button pops up on screen
         $(".boxButton").append(submitButton)
         ///TIMER FUNCTION.../////////////////////
-        $('.display').html("Seconds left: " +  timeleft)
+        $('.display').html("Seconds left: " + timeleft)
         intervalId = setInterval(count, 1000);
         function count() {
             /// we going to check our var timeleft, -1 every second, show its value on screen every second until its 0! 
@@ -35,9 +34,7 @@ $(document).ready(function () {
                 timeleft--;
                 $('.display').html("Seconds left: " + timeleft)
             } else {
-                // when its zero we gonna perform out "submit button function" to collect stats and show them, which i supposed to put to global scope
-                //and reuse here and when press submit button, i tried but im realy doing smthng wrong and its not working right...
-                //clearing interval, showing results
+                //if time is up we stop timer and colect stats (function described lower) in submit button section.
                 clearInterval(intervalId)
                 collectStats(userChoice, 0);
                 collectStats(userChoice2, 1);
@@ -50,24 +47,29 @@ $(document).ready(function () {
                     noAnswer++;
                 }
                 // we empty screen and showing results, if all right we say U GOT THEM ALL
-                // i should have put this in the function and reuse it again in else, but I am doing smthing wrong...
                 $('.boxButton').empty()
+                // i should have put this in the function and reuse it again in "else", but I am doing smthing wrong and its not working
+                //checking if user got all the right answers to show him WINNER screen
                 if ((userChoice == questions[0].answer) && (userChoice2 == questions[1].answer) && (userChoice3 == questions[2].answer)) {
                     var finalScreen = $('<div>')
                     var winner = $("<h1>").text("!!!YOU GOT THEM ALL!!!")
                     finalScreen.append(winner)
+                    //adding our stats to "finalScreen" div
                     var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
                         " You have " + wrongAnswers + " answers! " + " You left " + noAnswer + " with no answer! ")
                     finalScreen.append(stats)
+                    //putting our var finalScreen on computer screen.
                     $("#finalscreen").append(finalScreen)
                 }
                 else {
+                    //if user made one mistake he gets loser screen
                     var finalScreen = $('<div>')
                     var loser = $("<h1>").text("!!!TRY AGAIN!!!")
                     finalScreen.append(loser)
                     var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
                         " You have " + wrongAnswers + " wrong answers! " + " You left " + noAnswer + " with no answer! ")
                     finalScreen.append(stats)
+                    //putting our var finalScreen on computer screen.
                     $("#finalscreen").append(finalScreen)
                 }
 
@@ -76,37 +78,39 @@ $(document).ready(function () {
 
         $(this).hide()
         //QUestion one!
-        var questionDiv = $('<div>').attr("class","questionStyle")
+        // putting 1st question on screen, adding class to modify look
+        var questionDiv = $('<div>').attr("class", "questionStyle")
         $(".boxButton").append(questionDiv)
+        //adding our question and input buttons with some span to modify look and value to get on click and save in var userChoice
         $(questionDiv).html(questions[0].question + '<br>' +
             '<input id="first" type="radio" name="question1" value="Yes"> <span id="answerStyle">Yes</span>    |' +
             ' <span id="answerStyle">No</span>  <input id="first" type="radio" name="question1" value="No">  ')
+        //  getin value from radio button on click!
         $("input[id='first']").on("click", function () {
-            //  get value from radio button.
             userChoice = $(this).attr("value")
             console.log(userChoice)
 
         })
         //QUestion two!
-        var questionDiv1 = $('<div>').attr("class","questionStyle")
+        var questionDiv1 = $('<div>').attr("class", "questionStyle")
         $(".boxButton").append(questionDiv1)
         $(questionDiv1).html(questions[1].question + '<br>' +
             '<input id="second" type="radio" name="question2" value="Yes"> <span id="answerStyle">Yes</span>     |' +
             ' <span id="answerStyle">No</span>  <input id="second" type="radio" name="question2" value="No"> ')
+        //  getin value from radio button on click!            
         $("input[id='second']").on("click", function () {
-            // get value from radio button.
             userChoice2 = $(this).attr("value")
             console.log(userChoice2)
         })
         //Question three!
-        var questionDiv2 = $('<div>').attr("class","questionStyle")
+        var questionDiv2 = $('<div>').attr("class", "questionStyle")
         $(".boxButton").append(questionDiv2)
         $(questionDiv2).html(questions[2].question + '<br>' +
             '<input id="third" type="radio" name="question3" value="4"> <span id="answerStyle">4</span> ' +
             '<input id="third" type="radio" name="question3" value="5"> <span id="answerStyle">5</span> ' +
             '<input id="third" type="radio" name="question3" value="14"><span id="answerStyle">15</span>')
+        //  getin value from radio button on click!            
         $("input[id='third']").on("click", function () {
-            // get value from radio button.
             userChoice3 = $(this).attr("value")
             console.log(userChoice3)
             // console.log("right: "+rightAnswers)
@@ -114,15 +118,15 @@ $(document).ready(function () {
             // console.log("no answer: "+ noAnswer)
         })
     })
-
-    //SUBMIT BUTTON//
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    ////////////////////////////SUBMIT BUTTON////////////////////////////////
+    //creating submit button,adding text "submit"
     var submitButton = $("<button>").text('Submit')
-    // $(submitButton).on("click",
+    //when user click on "submit button"
     $(submitButton).on("click", function () {
+        // stop the timer
         clearInterval(intervalId);
         console.log("help")
+        //Collect all stats
         collectStats(userChoice, 0);
         collectStats(userChoice2, 1);
         if (userChoice3 == questions[2].answer) {
@@ -132,17 +136,20 @@ $(document).ready(function () {
         } else {
             noAnswer++;
         }
+        //hide everything that was displayed before
         $('.boxButton').empty()
+        //checking if user got all the right answers to show him WINNER screen with stats
         if ((userChoice == questions[0].answer) && (userChoice2 == questions[1].answer) && (userChoice3 == questions[2].answer)) {
             var finalScreen = $('<div>')
             var winner = $("<h1>").text("!!!!!!YOU GOT THEM ALL!!!!!!")
             finalScreen.append(winner)
-            var stats = $("<div>").html("You have " +  rightAnswers + " right answers! " +
+            var stats = $("<div>").html("You have " + rightAnswers + " right answers! " +
                 " You have " + wrongAnswers + " answers! " + " You left " + noAnswer + " with no answer! ")
             finalScreen.append(stats)
             $("#finalscreen").append(finalScreen)
         }
         else {
+            // if not then show him Loser Screen wish stats
             var finalScreen = $('<div>')
             var loser = $("<h1>").text("!!!TRY AGAIN!!!!")
             finalScreen.append(loser)
@@ -152,6 +159,8 @@ $(document).ready(function () {
             $("#finalscreen").append(finalScreen)
         }
     })
+    //stats collecting function, it checks input value that we keep in useChoice to question answer that we declared
+    //at the begining, if it matches it will add +1 to counters, so we know how many right,wrong,nonswers
     function collectStats(userchoice, i) {
         if (userchoice == questions[i].answer) {
             rightAnswers++;
@@ -164,23 +173,6 @@ $(document).ready(function () {
         }
     }
 })
-
-    //Submitting answers
-    // когда юзер нажал сабмит:
-    // -спрятать екран с вопросами
-    // -на чистом екране показать
-    //     -если все ответы правильные то показать на екране ( ВСЕ ПРАВИЛЬНО, ТЫ ПОБЕДИЛ!)
-    //         - кол-во правильных ответов.
-    //         - кол-во не правильных ответов.
-    //         -кол-во не отвеченых ответов
-    //     -если не все ответы правильны то показать (Ты проиграл!)
-    //         - кол-во правильных ответов.
-    //         - кол-во не правильных ответов.
-    //         -кол-во не отвеченых ответов
-
-
-
-
 
 
 
